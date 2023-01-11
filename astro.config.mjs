5619f5e8708c94b4a6f4402f135299ad9189259a
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-01-11 12:11:51 -0600

    adds tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,20 @@
 import { defineConfig } from "astro/config";
 import storyblok from "@storyblok/astro";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    storyblok({
-      accessToken: "LSTMzttTLabrQLn9YBzQowtt",
-      bridge: true,
-      apiOptions: { region: "us" },
-      components: {
-        page: "storyblok/Page",
-        feature: "storyblok/Feature",
-        grid: "storyblok/Grid",
-        teaser: "storyblok/Teaser",
-      },
-    }),
-  ],
+  integrations: [storyblok({
+    accessToken: "LSTMzttTLabrQLn9YBzQowtt",
+    bridge: true,
+    apiOptions: {
+      region: "us"
+    },
+    components: {
+      page: "storyblok/Page",
+      feature: "storyblok/Feature",
+      grid: "storyblok/Grid",
+      teaser: "storyblok/Teaser"
+    }
+  }), tailwind()]
 });
