e5f59d1e859216e1c121ae634a035d95dfeaa178
Sandra Rodgers <sandra.rodgers@deepgram.com>
2023-01-26 14:01:54 -0600

    removed comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,24 +3,23 @@ import storyblok from "@storyblok/astro";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/functions";
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   output: "server",
-  // SSR
   adapter: netlify(),
-  integrations: [storyblok({
-    accessToken: "LSTMzttTLabrQLn9YBzQowtt",
-    bridge: true,
-    apiOptions: {
-      region: "us"
-    },
-    components: {
-      page: "storyblok/Page",
-      feature: "storyblok/Feature",
-      grid: "storyblok/Grid",
-      teaser: "storyblok/Teaser"
-    }
-  }), tailwind()]
+  integrations: [
+    storyblok({
+      accessToken: "LSTMzttTLabrQLn9YBzQowtt",
+      bridge: true,
+      apiOptions: {
+        region: "us",
+      },
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+      },
+    }),
+    tailwind(),
+  ],
 });
